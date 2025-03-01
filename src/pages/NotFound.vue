e89027f9b8362b272cb012849c15a250d46e397e
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2025-03-01 13:07:15 -0300

    feat: add not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useLocale } from "@/composables/use-locale";
+
+const { t } = useLocale();
+</script>
+
+<template>
+  <div class="bg-c-blue-900 min-h-screen font-kumbh-sans flex justify-center">
+    <div class="text-white flex flex-col gap-4 items-center justify-center">
+      <h1 class="text-4xl font-semibold text-white max-s-480:text-2xl text-center">404 - {{ t('page-not-found') }}</h1>
+      <router-link to="/" class="underline text-c-blue-500">{{ t('go-back-to-home') }}</router-link>
+    </div>
+  </div>
+</template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import Home from "@/pages/Home.vue";
 import type { RouteRecordRaw } from "vue-router";
+import NotFound from "./pages/NotFound.vue";
 
 export const routes: Readonly<RouteRecordRaw[]> = [
   {
     path: "/",
     component: Home,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: NotFound,
   },
 ];
